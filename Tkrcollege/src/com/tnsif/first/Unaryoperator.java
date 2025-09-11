ed44da2e4ef609486ff7f797e7783e7b539ffc2c
kuruv <kuruv@lakshmiprasanna>
2025-09-11 18:31:17 +0530

    unaryoperator

---
+++ Tkrcollege/src/com/tnsif/first/Unaryoperator.java
@@ -0,0 +1,20 @@
+package com.tnsif.first;
+
+public class Unaryoperator {
+
+	public static void main(String[] args) {
+		
+		        int a = 5;
+		        boolean flag = false;
+
+		        System.out.println("++a = " + (++a));  // pre-increment
+		        System.out.println("a-- = " + (a--));  // post-decrement
+		        System.out.println("After a--, a = " + a);
+		        System.out.println("!flag = " + (!flag));
+		    
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
