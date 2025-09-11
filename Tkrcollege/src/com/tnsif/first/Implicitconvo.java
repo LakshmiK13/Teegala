015b9186afc4ce5fd71a47c82358f9e84a43a4e6
kuruv <kuruv@lakshmiprasanna>
2025-09-11 19:21:44 +0530

    implicit convo

---
+++ Tkrcollege/src/com/tnsif/first/Implicitconvo.java
@@ -0,0 +1,17 @@
+package com.tnsif.first;
+
+public class Implicitconvo {
+
+	public static void main(String[] args) {
+		
+		        int a = 10;          
+		        double b = a;       
+		        System.out.println("Integer a = " + a);
+		        System.out.println("Converted to double b = " + b);
+		    
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
