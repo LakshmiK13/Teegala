a0c8414d2f065dd113c94c1461bf09d44d70ada6
kuruv <kuruv@lakshmiprasanna>
2025-09-11 18:36:58 +0530

    assignment operator

---
+++ Tkrcollege/src/com/tnsif/first/Assignmentoper.java
@@ -0,0 +1,20 @@
+package com.tnsif.first;
+
+public class Assignmentoper {
+
+	public static void main(String[] args) {
+	
+		        int x = 10;
+		        System.out.println("x = " + x);
+		        x += 5;  // x = x + 5
+		        System.out.println("x += 5 → " + x);
+		        x *= 2;  // x = x * 2
+		        System.out.println("x *= 2 → " + x);
+		        x -= 4;  // x = x - 4
+		        System.out.println("x -= 4 → " + x);
+		   
+		// TODO Auto-generated method stub
+
+	}
+
+}
