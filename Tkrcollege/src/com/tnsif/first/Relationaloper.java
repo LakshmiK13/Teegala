73815c148a19e6d0a1a196ffd36f3dc8cd683792
kuruv <kuruv@lakshmiprasanna>
2025-09-11 18:44:27 +0530

    relational operators

---
+++ Tkrcollege/src/com/tnsif/first/Relationaloper.java
@@ -0,0 +1,18 @@
+package com.tnsif.first;
+
+public class Relationaloper {
+
+	public static void main(String[] args) {
+	
+		        int a = 10, b = 20;
+		        System.out.println("a == b → " + (a == b));
+		        System.out.println("a != b → " + (a != b));
+		        System.out.println("a > b → " + (a > b));
+		        System.out.println("a < b → " + (a < b));
+		        System.out.println("a >= 10 → " + (a >= 10));
+		    
+		// TODO Auto-generated method stub
+
+	}
+
+}
