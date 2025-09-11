dbd889b216f9c37f90f3bf42aa51bd1f9733b167
kuruv <kuruv@lakshmiprasanna>
2025-09-11 19:18:35 +0530

    explicit convo

---
+++ Tkrcollege/src/com/tnsif/first/Explicitconvo.java
@@ -0,0 +1,15 @@
+package com.tnsif.first;
+
+public class Explicitconvo {
+
+	public static void main(String[] args) {
+		
+		        double x = 10.75;      
+		        int y = (int) x;       
+		        System.out.println("Double x = " + x);
+		        System.out.println("Converted to int y = " + y);  
+		// TODO Auto-generated method stub
+
+	}
+
+}
