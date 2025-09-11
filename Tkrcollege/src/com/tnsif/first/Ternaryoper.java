67a08e771b4fa820a24045ca560321de7ebce06e
kuruv <kuruv@lakshmiprasanna>
2025-09-11 19:13:04 +0530

    ternary oper

---
+++ Tkrcollege/src/com/tnsif/first/Ternaryoper.java
@@ -0,0 +1,15 @@
+package com.tnsif.first;
+
+public class Ternaryoper {
+
+	public static void main(String[] args) {
+	
+		        int num = 8;
+		        String result = (num % 2 == 0) ? "Even" : "Odd";
+		        System.out.println(num + " is " + result);
+		   
+		// TODO Auto-generated method stub
+
+	}
+
+}
