e282ca1ed439b46264b17d23746f96d6cd6c5ad3
kuruv <kuruv@lakshmiprasanna>
2025-09-11 19:02:30 +0530

    bitwise operator

---
+++ Tkrcollege/src/com/tnsif/first/Bitwiseoper.java
@@ -0,0 +1,19 @@
+package com.tnsif.first;
+
+public class Bitwiseoper {
+
+	public static void main(String[] args) {
+		
+		        int a = 5, b = 3;
+		        System.out.println("a & b = " + (a & b)); 
+		        System.out.println("a | b = " + (a | b)); 
+		        System.out.println("a ^ b = " + (a ^ b)); 
+		        System.out.println("~a = " + (~a));       
+		        System.out.println("a << 1 = " + (a << 1)); 
+		        System.out.println("a >> 1 = " + (a >> 1)); 
+		    
+		// TODO Auto-generated method stub
+
+	}
+
+}
