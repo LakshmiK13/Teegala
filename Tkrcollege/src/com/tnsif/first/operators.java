0ed86fe3175815ca1e2845ef3a729cc328ff40c1
kuruv <kuruv@lakshmiprasanna>
2025-09-11 17:59:00 +0530

    operators

---
+++ Tkrcollege/src/com/tnsif/first/operators.java
@@ -0,0 +1,15 @@
+package com.tnsif.first;
+
+public class operators {
+	    public static void main(String[] args) {
+	        int a = 10, b = 3;
+	        System.out.println("a + b = " + (a + b));
+	        System.out.println("a - b = " + (a - b));
+	        System.out.println("a * b = " + (a * b));
+	        System.out.println("a / b = " + (a / b));
+	        System.out.println("a % b = " + (a % b));
+	    }
+	
+
+
+}
