505ed075449fe544d87121569e3ddb966fa193aa
kuruv <kuruv@lakshmiprasanna>
2025-09-11 18:56:20 +0530

    logical opertor

--- Tkrcollege/src/com/tnsif/first/Logicaloper.java
+++ Tkrcollege/src/com/tnsif/first/Logicaloper.java
@@ -3,6 +3,13 @@ package com.tnsif.first;
 public class Logicaloper {
 
 	public static void main(String[] args) {
+		
+		        int a = 15, b = 5;
+		        System.out.println("(a > 10 && b < 10) → " + (a > 10 && b < 10));
+		        System.out.println("(a < 10 || b < 10) → " + (a < 10 || b < 10));
+		        System.out.println("!(a > b) → " + (!(a > b)));
+		   
+
 		// TODO Auto-generated method stub
 
 	}
