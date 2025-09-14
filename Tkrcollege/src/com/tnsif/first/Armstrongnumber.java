d5823a8b97d63d921baa822e47084d693899ea78
kuruv <kuruv@lakshmiprasanna>
2025-09-14 20:24:38 +0530

    armstrong

--- Tkrcollege/src/com/tnsif/first/Armstrongnumber.java
+++ Tkrcollege/src/com/tnsif/first/Armstrongnumber.java
@@ -4,28 +4,21 @@ public class Armstrongnumber {
 
 	public static void main(String[] args) {
 		
-		        int num = 153;   // you can change this value
+		        int num = 13331;   // you can change this value
 		        int original = num;
-		        int sum = 0;
+		        int reversed = 0;
 
 		        while (num > 0) {
-		            int digit = num % 10;         // get last digit
-		            sum = sum + (digit * digit * digit); // cube of digit
-		            num = num / 10;               // remove last digit
+		            int digit = num % 10;          // get last digit
+		            reversed = (reversed * 10) + digit; // build reversed number
+		            num = num / 10;                // remove last digit
 		        }
 
-		        if (sum == original) {
-		            System.out.println(original + " is an Armstrong number.");
+		        if (original == reversed) {
+		            System.out.println(original + " is a Palindrome number.");
 		        } else {
-		            System.out.println(original + " is not an Armstrong number.");
+		            System.out.println(original + " is not a Palindrome number.");
 		        }
 		    }
 		}
 
-		       
-		
-		        
-		 
-
-
-	
