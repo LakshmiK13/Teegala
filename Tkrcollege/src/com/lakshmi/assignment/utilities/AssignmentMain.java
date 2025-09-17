9729b4ada448afae62442560de5da196f89d72a9
kuruv <kuruv@lakshmiprasanna>
2025-09-17 21:18:46 +0530

    all packages

---
+++ Tkrcollege/src/com/lakshmi/assignment/utilities/AssignmentMain.java
@@ -0,0 +1,28 @@
+package com.lakshmi.assignment.utilities;
+
+	import com.lakshmi.assignment.employees.Manager;
+	import com.lakshmi.assignment.employees.Developer;
+	import com.lakshmi.assignment.utilities.EmployeeUtilities;
+
+	/**
+	 * Main class to test Employee system
+	 */
+	public class AssignmentMain {
+	    public static void main(String[] args) {
+	        // Create Manager and Developer objects
+	        Manager m = new Manager("Alice", 101, 80000, "HR");
+	        Developer d = new Developer("Bob", 102, 60000, "Java");
+
+	        // Use EmployeeUtilities
+	        EmployeeUtilities util = new EmployeeUtilities();
+
+	        System.out.println("=== Manager Info ===");
+	        util.displayEmployeeInfo(m);
+	        System.out.println("Department: " + m.getDepartment());
+
+	        System.out.println("\n=== Developer Info ===");
+	        util.displayEmployeeInfo(d);
+	        System.out.println("Programming Language: " + d.getProgrammingLanguage());
+	    }
+	}
+
---
+++ Tkrcollege/src/com/lakshmi/assignment/utilities/EmployeeUtilities.java
@@ -0,0 +1,13 @@
+package com.lakshmi.assignment.utilities;
+
+import com.lakshmi.assignment.employees.Employee;
+	public class EmployeeUtilities {
+
+	    // Method to display employee details
+	    public void displayEmployeeInfo(Employee e) {
+	        System.out.println("Employee ID: " + e.getEmployeeId());
+	        System.out.println("Name: " + e.getName());
+	        System.out.println("Salary: " + e.getSalary());
+	   
+	}
+}
