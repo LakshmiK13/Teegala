02fbdf9fbe1a7efa27f994c9ec2268535449add1
kuruv <kuruv@lakshmiprasanna>
2025-10-10 23:31:53 +0530

    commit spring

---
+++ employee/src/main/java/repo/EmpRepo.java
@@ -0,0 +1,11 @@
+package repo;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import entity.Employee;
+
+public interface EmpRepo extends JpaRepository<Employee, Integer> {
+    // Marker Interface (no code needed)
+}
+
+
---
+++ employee/src/main/java/service/EmpService.java
@@ -0,0 +1,41 @@
+package service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import entity.Employee;
+import repo.EmpRepo;
+import java.util.List;
+
+@Service
+public class EmpService {
+
+    @Autowired
+    private EmpRepo repo;
+
+    // CREATE
+    public Employee addEmployee(Employee e) {
+        return repo.save(e);
+    }
+
+    // READ
+    public List<Employee> getAllEmployees() {
+        return repo.findAll();
+    }
+
+    public Employee getEmployeeById(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    // UPDATE
+    public Employee updateEmployee(Employee e) {
+        return repo.save(e);
+    }
+
+    // DELETE
+    public String deleteEmployee(int id) {
+        repo.deleteById(id);
+        return "Employee removed with ID: " + id;
+    }
+}
+
