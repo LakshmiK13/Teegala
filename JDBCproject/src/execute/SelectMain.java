096f3ae620f31d6d593b8a3b674b325dd9c0c900
kuruv <kuruv@lakshmiprasanna>
2025-10-10 22:22:43 +0530

    commit jdbc

---
+++ JDBCproject/src/execute/SelectMain.java
@@ -0,0 +1,13 @@
+package execute;
+import Services.Crudoperations;
+
+public class SelectMain {
+
+	public static void main(String[] args) {
+		Crudoperations.displayAllStudents();
+		Crudoperations.searchStudent(101);
+
+	}
+
+}
+
